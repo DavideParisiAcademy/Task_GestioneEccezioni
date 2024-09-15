821b9fbc46dcb9ddb74abbc0792b9779b3bf6fa2
DavideParisiAcademy <d.parisi@iemmeconsulting.it>
2024-09-15 21:12:58 +0200

    version 1.0

---
+++ Task_GestioneEccezioni/Classes/GestioneIO.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_GestioneEccezioni.Classes
+{
+    internal class GestioneIO
+    {
+        public string[]? fileTxt { get; set; }
+
+        //costruttore di default
+
+        //methods
+
+        public void InserimentoFile(string? path)
+        {
+            try
+            {
+                fileTxt = File.ReadAllLines(path);
+                Console.WriteLine("File caricato");
+                Console.WriteLine("---------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void LetturaFile()
+        {
+            try
+            {
+                if (fileTxt?.Length != 0)
+                {
+                    for (int i = 0; i < fileTxt?.Length; i++)
+                    {
+                        Console.WriteLine(fileTxt[i]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERRORE: il file è vuoto");
+                    Console.WriteLine("--------------------");
+                }
+                Console.WriteLine("---------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void SommaNumeri()
+        {
+            double ris = 0;
+            try
+            {
+                for (int i = 0; i < fileTxt?.Length; i++)
+                {
+                    ris = ris + Convert.ToDouble(fileTxt[i]);
+                }
+                Console.WriteLine($"Risultato: {ris}");
+                Console.WriteLine("------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
---
+++ Task_GestioneEccezioni/Program.cs
@@ -0,0 +1,45 @@
+﻿using Task_GestioneEccezioni.Classes;
+
+namespace Task_GestioneEccezioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GESTIONE FILE DI TESTO");
+
+            bool ciclo = true;
+            GestioneIO gs = new GestioneIO();
+
+            while (ciclo)
+            {
+                
+                string? path;
+                Console.WriteLine("Inserisci un comando\n1-Carica file\n2-Leggi file\n3-Somma numeri\n4- Esci");
+                string? inputUtente = Console.ReadLine();
+                
+
+                switch (inputUtente)
+                {
+                    case "1":
+                        Console.WriteLine("Inserisci il path del file da caricare");
+                        path = Console.ReadLine();
+                        gs.InserimentoFile(path);
+                        break;
+                    case "2":
+                        Console.WriteLine("[Contenuto del file]:");
+                        gs.LetturaFile();
+                        break;
+                    case "3":
+                        gs.SommaNumeri();
+                        break;
+                    case "4":
+                        ciclo = false;
+                        break;
+                    default: Console.WriteLine("ERRORE DI INSERIMENTO");
+                        break;
+                }
+            }            
+        }
+    }
+}
